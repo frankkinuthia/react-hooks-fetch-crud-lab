50ea6dcf8175c52f031b23316b8c6e66bf41518d
+ <+>
2023-01-18 11:13:23 +0300

    Done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,30 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+ 
+  function handleDeleteQuestion(deleteQuestion) {
+    const updateQuestions = questions.filter(
+      (question) => question.id !== deleteQuestion.id
+    );
+
+    setQuestions(updateQuestions);
+  }
+
+  const questionList = questions.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        onDeleteQuestion={handleDeleteQuestion}
+        question={question}
+      />
+    );
+  });
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
